30de3fbd497c9efe91893cd5df7550033f35d6e0
Carlos Ferrer <Carlos.Alberto@Sci-us.com>
2022-07-31 18:22:44 -0300

    first commit

---
+++ vue-progresive/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2> {{ text }}</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam corporis a eius harum recusandae officia ex perspiciatis quibusdam quod, eligendi accusamus. Excepturi est sunt dolores atque perferendis consequatur similique dicta.</p>
+    </div>
+</template>
+
+
+
+<script>
+export default{
+    data(){
+        return{
+            text: "hola vue"
+        }
+    },
+};
+</script>
+
+<style>
+    h2{
+        color: red;
+    }
+</style>
